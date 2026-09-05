83306921ab8435ab47cfe88810758892ea3edeb2
AnnaneLabs <software.engineer.ma@proton.me>
2026-09-05 18:35:55 +0100

    Variables and data types in java

---
+++ Day01/Exercice1_Variables.java
@@ -0,0 +1,45 @@
+/**
+ * ============================================
+ *  EXERCICE 1 — Variables et types
+ * ============================================
+ *
+ * Énoncé :
+ * --------
+ * Créer les variables suivantes :
+ * - nom (String)
+ * - prenom (String)
+ * - age (int)
+ * - salaire (double)
+ * - actif (boolean)
+ *
+ * Puis afficher toutes les informations.
+ *
+ * Exemple de sortie :
+ * -------------------
+ * Nom : Belfqir
+ * Prénom : Ahmed
+ * Age : 25
+ * Salaire : 15000.5
+ * Actif : true
+ */
+public class Exercice1_Variables {
+
+    public static void main(String[] args) {
+
+        // TODO : Déclarer les variables
+        String nom = "Belfqir";
+        String prenom = "Ahmed";
+        int age = 25;
+        double salaire = 15000.5;
+        boolean active = true;
+
+        // TODO : Afficher les informations
+
+        System.out.println("Nom :" + nom);
+        System.out.println("Prenom : " + prenom);
+        System.out.println("Age :" + age);
+        System.out.println("Salaire :" + salaire);
+        System.out.println("Actif :" + active);
+
+    }
+}
